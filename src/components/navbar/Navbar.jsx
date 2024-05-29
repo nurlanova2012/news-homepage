e08e5440a7d52f1273a76d4c3f70149c6239b69a
Aizirek Nurlanbekova <nurlanova2012@gmail.com>
2024-05-29 15:06:27 +0600

    final version

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav id="navbar">
       <div className="container container__navbar">
         <div className="logo">
           <a href="#">W.</a>
         </div>
-        <div className="nav__list">
+        <div className={`nav__list ${isMenuOpen ? "open" : ""}`}>
           <a href="#">Home</a>
           <a href="#New">New</a>
           <a href="#Popular">Popular</a>
           <a href="#Trending">Trending</a>
           <a href="#Categories">Categories</a>
         </div>
+        <div
+          className={`menu-icon ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        ></div>
       </div>
     </nav>
   );
